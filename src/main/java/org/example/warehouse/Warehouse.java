7ebf264087779d41289e619de2d629bb0af3febb
Kouroshkt <143129805+Kouroshkt@users.noreply.github.com>
2023-10-08 09:03:57 +0200

    Update

--- src/main/java/org/example/warehouse/Warehouse.java
+++ src/main/java/org/example/warehouse/Warehouse.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class Warehouse {
     private static Warehouse instance;
     private final String name;
-    private final Map<UUID, ProductRecord> productMap;
+    private  final Map<UUID, ProductRecord> productMap;
     private final List<ProductRecord> changedProducts;
 
     private Warehouse(String name) {
@@ -16,28 +16,34 @@ public class Warehouse {
         this.productMap = new HashMap<>();
         this.changedProducts = new ArrayList<>();
     }
+
     public static Warehouse getInstance() {
         if (instance == null) instance = new Warehouse("MyStore");
         return instance;
     }
+
     public static Warehouse getInstance(String name) {
         return new Warehouse(name);
     }
+
     public boolean isEmpty() {
         return productMap.isEmpty();
     }
+
     public ProductRecord addProduct(UUID id, String name, Category category, BigDecimal price) {
-        if (productMap.containsKey(id)) throw new IllegalArgumentException("Product with that id already exists, use updateProduct for updates.");
+        if (productMap.containsKey(id))
+            throw new IllegalArgumentException("Product with that id already exists, use updateProduct for updates.");
         if (name == null || name.isEmpty()) throw new IllegalArgumentException("Product name can't be null or empty.");
         if (category == null) throw new IllegalArgumentException("Category can't be null.");
-        if (price==null) price = BigDecimal.ZERO;
+        if (price == null) price = BigDecimal.ZERO;
         ProductRecord productRecord = new ProductRecord(id, name, category, price);
         productMap.put(id, productRecord);
         return productRecord;
     }
+
     public void updateProductPrice(UUID id, BigDecimal newPrice) {
         if (!productMap.containsKey(id)) throw new IllegalArgumentException("Product with that id doesn't exist.");
-        else if(newPrice==null) newPrice=BigDecimal.ZERO;
+        else if (newPrice == null) newPrice = BigDecimal.ZERO;
         else {
             ProductRecord productRecord = productMap.get(id);
             productRecord.setPrice(newPrice);
@@ -48,6 +54,7 @@ public class Warehouse {
     public List<ProductRecord> getProducts() {
         return Collections.unmodifiableList(new ArrayList<>(productMap.values()));
     }
+
     public Optional<ProductRecord> getProductById(UUID id) {
         return Optional.ofNullable(productMap.get(id));
     }
